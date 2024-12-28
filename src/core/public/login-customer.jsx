02179f642401cc3021dbf75ed72158fbc01698ab
bipasha <bipashalamsal@gmail.com>
2024-12-28 15:50:59 +0545

    /public - /login-customer design changed

--- src/core/public/login-customer.jsx
+++ src/core/public/login-customer.jsx
@@ -53,11 +53,11 @@ const CustomerLogin = () => {
         <h1 className="text-2xl font-medium text-center text-black mb-6">Sign In your Account</h1>
         <form onSubmit={handleLogin}>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-black font-normal mb-2">Email Address</label>
+            <label htmlFor="email" className="block text-black text-sm font-medium mb-2">Email Address</label>
             <input
               type="email"
               id="email"
-              className={`w-full p-3 border ${error.email ? 'border-red-500' : 'border-gray-300'} rounded-lg focus:outline-none focus:ring-2 focus:ring-white`}
+              className={`w-full py-2 border ${error.email ? 'border-red-500' : 'border-gray-300'} rounded-lg focus:outline-none focus:ring-2 focus:ring-white`}
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -66,11 +66,11 @@ const CustomerLogin = () => {
           </div>
 
           <div className="mb-4 relative">
-            <label htmlFor="password" className="block text-black font-normal mb-2">Password</label>
+            <label htmlFor="password" className="block text-black text-sm font-medium mb-2">Password</label>
             <input
               type={showPassword ? "text" : "password"}
               id="password"
-              className={`w-full p-3 border ${error.password ? 'border-red-500' : 'border-gray-300'} rounded-lg focus:outline-none focus:ring-2 focus:ring-white`}
+              className={`w-full py-2 border ${error.password ? 'border-red-500' : 'border-gray-300'} rounded-lg focus:outline-none focus:ring-2 focus:ring-white`}
              
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -88,37 +88,40 @@ const CustomerLogin = () => {
 
           <button
             type="submit"
-            className={`w-full bg-[#80CBB2] py-2 rounded-md text-white font-semibold hover:bg-[#90c9b8] focus:outline-none focus:ring focus:ring-red-600 ${isLoading ? "opacity-50 cursor-not-allowed" : ""}`}
+            className={`w-full bg-[#80CBB2] py-2 rounded-lg text-white font-semibold hover:bg-[#90c9b8] focus:outline-none focus:ring focus:ring-red-600 ${isLoading ? "opacity-50 cursor-not-allowed" : ""}`}
             disabled={isLoading}
           >
             {isLoading ? "Processing..." : "Sign In"}
           </button>
         </form>
 
-        <div className="flex items-center justify-center mt-4">
-          <span className="text-gray-400">----------------------------------OR -----------------------------------</span>
+        <div className="flex items-center text-xs justify-center mt-4">
+          <hr className="border-t border-dashed border-gray-500 w-1/2 mr-2" />
+          <span className="text-gray-500 ">OR</span>
+          <hr className="border-t border-dashed border-gray-500 w-1/2 ml-2" />
         </div>
 
+
         <div className="mt-4">
-          <button className="w-full bg-white py-3 rounded-3xl text-black font-semibold focus:outline-none focus:ring"
+          <button className="w-full text-sm bg-white py-2 rounded-3xl text-black font-semibold focus:outline-none focus:ring"
           style={{ border: "1px solid #80CBB2" }}>
             Sign In with a Code
           </button>
         </div>
 
         <div className="mt-4 text-center">
-          <a href="#" className="text-gray-400 hover:text-gray-200 text-sm">
+          <a href="#" className="text-gray-500 hover:text-gray-400 text-xs">
             Forgot Password?
           </a>
         </div>
 
         <div className="mt-4">
-          <label className="inline-flex items-center">
+          <label className="inline-flex items-center text-xs">
             <input
               type="checkbox"
-              className="form-checkbox h-5 w-5 text-red-600"
+              className="form-checkbox h-4 w-4"
             />
-            <span className="ml-2 text-gray-400">Remember Me</span>
+            <span className="ml-2 text-gray-500 ">Remember Me</span>
           </label>
         </div>
       </div>
